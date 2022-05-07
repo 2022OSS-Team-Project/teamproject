f52aa5dd266721dea9300d6a7a46b84906c98e31
strawberry-flavored-milk <hikyj1701@gmail.com>
2022-05-07 18:04:12 +0900

    Update membership_management.c

---
+++ miniproject/membership_management.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "inc/personal_info.h"
+
+int main() {
+    Person *p[20];
+    int index = 0;
+    int count = 0;
+    int menu;
+
+    while(1) {
+        menu = selectMenu();
+        if(menu == 0) break;
+        if(menu == 1) {
+            if(count > 0) {
+                listMembership(p, index);
+            }
+            else
+                printf("데이터가 없습니다.");
+        }
+        else if(menu == 2) {
+            p[index] = (Person *)malloc(sizeof(Person));
+            count += createInfo(p[index++]);
+        }
+    }
+
+    return 0;
+}
