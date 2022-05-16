ad3b5e5ee59cb5e89792186ebaa85876ea475b3f
strawberry-flavored-milk <hikyj1701@gmail.com>
2022-05-16 16:29:53 +0900

    Update personal_info.h

--- personal_info.h
+++ personal_info.h
@@ -11,8 +11,8 @@ typedef struct {
     int endDay;
 } Person;
 
-int selectMenu (); //메뉴 선택 함수
-void listMembership (Person *p, int count); //현재 추가된 항목들 리스트하기
+int selectMenu ();
+void listMembership (Person *p, int count);
 int selectDataNo (Person *p, int count);
 int createInfo (Person *p);
 void readInfo (Person *p);
@@ -22,7 +22,7 @@ void saveInfoFile (Person p[], int count);
 int readInfoFile (Person *p);
 int selectReadInfo (Person *p);
 int calculateInbody (Person *p);
-int underWeight (Person *p);
-int normal (Person *p);
-int overWeight (Person *p);
+void underWeight (Person *p);
+void normal (Person *p);
+void overWeight (Person *p);
 int obese (Person *p);
